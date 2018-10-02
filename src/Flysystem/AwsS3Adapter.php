35780cb5ac4995d8509dfadeabc0168775537be3
Damian Lee <damiankloip@gmail.com>
2018-10-02 11:05:43 +0100

    Fix S3 URL generation

--- src/Flysystem/AwsS3Adapter.php
+++ src/Flysystem/AwsS3Adapter.php
@@ -23,19 +23,22 @@ class AwsS3Adapter extends FlysystemAwsS3Adapter
         $expires = $this->convertToTimestamp($expiration);
 
         $client = $this->getClient();
-        $awsKeyId = $client->getConfig('key');
-        $awsSecret = $client->getConfig('secret');
-        $awsRegion = $client->getConfig('region');
+        $credentials = $client->getCredentials()->wait();
+
+        $awsKeyId = $credentials->getAccessKeyId();
+        $awsSecret = $credentials->getSecretKey();
+        $awsRegion = $client->getConfig('signing_region');
+        $full_path = $this->applyPathPrefix($path);
 
         $amzHeaders = '';
-        $amzResource = sprintf('/%s/%s', $bucket, $path);
-        $request = sprintf("GET\n\n\n%s\n%s%s", $expiration, $amzHeaders, $amzResource);
+        $amzResource = sprintf('/%s/%s', $bucket, $full_path);
+        $request = sprintf("GET\n\n\n%s\n%s%s", $expires, $amzHeaders, $amzResource);
 
         $hmac = hash_hmac('sha1', $request, $awsSecret, true);
         $base64hmac = urlencode(base64_encode($hmac));
 
         $url = 'https://%s.s3.%s.amazonaws.com/%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s';
-        return sprintf($url, $bucket, $awsRegion, $path, $awsKeyId, $expires, $base64hmac);
+        return sprintf($url, $bucket, $awsRegion, $full_path, $awsKeyId, $expires, $base64hmac);
     }
 
     /**
